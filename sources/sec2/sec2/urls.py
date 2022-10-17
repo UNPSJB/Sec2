cdcd339780c758f4f7621587d20d64dfb62696c1
Diego <diegoezequielfabro@gmail.com>
2022-10-17 16:42:29 -0300

    sigo con afiliado

--- sources/sec2/sec2/urls.py
+++ sources/sec2/sec2/urls.py
@@ -1,4 +1,5 @@
-"""sec2 URL Configuration
+"""
+sec2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
