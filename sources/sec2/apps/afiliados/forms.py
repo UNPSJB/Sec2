909e0cba6505169233162c42e25f4bb7b7546725
carlos <lucero.carlos.ch@gmail.com>
2022-10-22 08:37:55 -0300

    se agrego informacion a formulario afiliado

--- sources/sec2/apps/afiliados/forms.py
+++ sources/sec2/apps/afiliados/forms.py
@@ -3,14 +3,51 @@ from pyexpat import model
 from tkinter.ttk import Widget
 from django import forms
 from .models import Afiliado
+from apps.personas.forms import PersonaForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, Div, HTML
 
 class FormularioAfiliado(forms.ModelForm):
-
+    
+    
+    
+    
     class Meta:
         model = Afiliado
         fields = '__all__'
+        exclude=['persona', 'tipo']
         Widgets ={
-            'fechaNacimiento': forms.DateInput(attrs={'type':'date'}),
+            'fechaNacimiento': forms.DateInput(attrs={'type':'datetime-local'}),
             'fechaIngresoTrabajo': forms.DateInput(attrs={'type':'date'}),
             'fechaAfiliacion': forms.DateInput(attrs={'type':'date'})
             }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = 'guardarAfiliado'
+        self.helper.layout = Layout(
+            Fieldset(
+                   "",
+                HTML(
+                    '<hr/>'),
+                    'dni', 
+                    'nombre',
+                    'apellido',
+                    'direccion',
+                    'mail',
+                    'nacionalidad',
+                    'estado_civil',
+                    'cuil',
+                    'cuil',
+                    'celular',
+                    'razon_social',
+                    'cuit',
+                    'categoria_laboral',        
+            
+            ),
+            Submit('submit', 'Submit', css_class='button white'),)
+
+
+FormularioAfiliado.base_fields.update(PersonaForm.base_fields)
+
