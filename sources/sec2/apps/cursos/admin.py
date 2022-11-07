f2316a2ba3507490a2ba1a7463c7c2d58eb78e47
Diego van Haaster <diegomvh@gmail.com>
2022-11-07 19:49:46 -0300

    Filtros

--- sources/sec2/apps/cursos/admin.py
+++ sources/sec2/apps/cursos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Actividad
 
 # Register your models here.
+class ActividadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'area')
+    list_filter = ('area',)
+    ordering = ('nombre', 'area')
+
+admin.site.register(Actividad, ActividadAdmin)
--- sources/sec2/apps/personas/admin.py
+++ sources/sec2/apps/personas/admin.py
@@ -5,7 +5,11 @@ from .models import Persona
 from .models import Rol
 
 # Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'fecha_nacimiento')
+    list_filter = ('nombre','apellido')
+    ordering = ('nombre', 'fecha_nacimiento')
 
-admin.site.register(Persona)
+admin.site.register(Persona, PersonaAdmin)
 
 
