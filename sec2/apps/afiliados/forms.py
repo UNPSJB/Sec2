89c0b92d09f62217825ce100b1288cd594f215fc
cozakoo <martinarcosvargas2@gmail.com>
2023-10-29 14:23:34 -0300

    modificacion de afiliado hecha pero no editable

--- sec2/apps/afiliados/forms.py
+++ sec2/apps/afiliados/forms.py
@@ -5,7 +5,8 @@ from tkinter.ttk import Widget
 from django import forms
 from django.forms import ValidationError
 from .models import Afiliado
-from apps.personas.forms import PersonaForm, PersonaUpdateForm
+from apps.personas.forms import PersonaForm
+from apps.personas.forms import PersonaUpdateForm
 from apps.personas.models import Persona
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Submit, Div, HTML
@@ -59,146 +60,7 @@ class AfiliadoPersonaForm(forms.ModelForm):
             raise forms.ValidationError("Debes ser mayor de 18 años y menor de 100 años.")
         return fecha_nacimiento
 
-    # def save(self, commit=True):
-    #     print("ESTOY EN EL SAVE")
-    #     afiliado = super().save(commit=False)
-    #     # Crear una instancia de Persona
-    #     persona = Persona(
-    #         dni=self.cleaned_data["dni"],
-    #         cuil=self.cleaned_data["cuil"],
-    #         nombre=self.cleaned_data["nombre"],
-    #         apellido=self.cleaned_data["apellido"],
-    #         fecha_nacimiento=self.cleaned_data["fecha_nacimiento"],
-    #         mail=self.cleaned_data["mail"],
-    #         celular=self.cleaned_data["celular"],
-    #         estado_civil=self.cleaned_data["estado_civil"],
-    #         nacionalidad=self.cleaned_data["nacionalidad"],
-    #         direccion=self.cleaned_data["direccion"],
-    #     )
-    #     persona.save()
-    #     # Asociar la persona al afiliado
-    #     afiliado.persona = persona
-    #     afiliado.save()
-        
-    #     return persona
-
-##------------------------------------------------
-# class AfiliadoForm(forms.ModelForm):
-#     class Meta:
-#         model = Afiliado
-#         fields = '__all__'
-#         exclude = ['persona', 'tipo', 'estado']
-#         widgets = {
-#             'fechaIngresoTrabajo': forms.DateInput(attrs={'type': 'date'}),
-#             'fechaAfiliacion': forms.DateInput(attrs={'type': 'date'})
-#         }
-#         labels = {
-#             'fechaIngresoTrabajo': "fecha de ingreso al trabajo",
-#             'fechaAfiliacion': "Fecha de afiliacion"
-#         }
-
-# class FormularioAfiliadoCreate(forms.ModelForm):
-#     fechaAfiliacion = forms.DateField()
-#     class Meta:
-#         model = Persona
-#         fields = '__all__'
-        
-#         widgets = {
-#             'fecha_nacimiento': forms.DateInput(attrs={'type': 'date', 'max': str(date.today())}),
-#             # 'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
-#         }
-#         labels = {
-#             'fecha_nacimiento': "Fecha de nacimiento",
-#         }
-#         fecha_nacimiento = forms.DateField(
-#             widget=forms.DateInput(attrs={'type': 'date'}),
-#         )
-
-#     def clean_fecha_nacimiento(self):
-#             fecha_nacimiento = self.cleaned_data.get('fecha_nacimiento')
-#             edad = date.today().year - fecha_nacimiento.year
-#             if edad < 18 or edad >= 100:
-#                 raise forms.ValidationError("Debes ser mayor de 18 años y menor de 100 años.")
-            
-#             return fecha_nacimiento
-#     # def clean_dni(self):
-#     #     dni=self.cleaned_data['dni']
-#     #     self.persona = Persona.objects.filter(
-#     #         dni).first()
-#     #     if len(dni) > 8 :
-#     #         print("DNI EXCEDE CARACTERES")
-#     #     if self.persona is not None and self.persona.es_afiliado:
-#     #         raise ValidationError("Ya existe un afiliado activo con ese DNI")
-#     #     return self.cleaned_data['dni']
-
-#     def is_valid(self) -> bool:
-#         valid = super().is_valid()
-#         # personaForm = PersonaForm(data=self.cleaned_data)
-#         return valid
-
-#     def save(self, commit=False):
-#         dni = self.cleaned_data["dni"]
-#         cuil = self.cleaned_data["cuil"]
-#         nombre = self.cleaned_data["nombre"]
-#         apellido = self.cleaned_data["apellido"]
-#         fechaN = self.cleaned_data["fecha_nacimiento"]       
-#         mail = self.cleaned_data["mail"]
-#         celular = self.cleaned_data["celular"]
-#         estadoCivil = self.cleaned_data["estado_civil"]
-#         nacionalidad = self.cleaned_data["nacionalidad"]
-#         direccion = self.cleaned_data["direccion"]
-
-#         pearson = Persona(dni=dni,
-#                         fecha_nacimiento=fechaN,
-#                         cuil= cuil,
-#                         nombre=nombre,
-#                         apellido=apellido,
-#                         direccion=direccion,
-#                         mail=mail,
-#                         celular=celular,
-#                         estado_civil=estadoCivil,
-#                         nacionalidad= nacionalidad)        
-#         self.persona = pearson.save()
-
-#         cuit_empleador = self.cleaned_data["cuit_empleador"]
-#         razon_social = self.cleaned_data["razon_social"]
-#         categoria_laboral = self.cleaned_data["categoria_laboral"]
-#         domicilio_empresa = self.cleaned_data["domicilio_empresa"]
-#         localidad_empresa = self.cleaned_data["localidad_empresa"]
-#         rama = self.cleaned_data["rama"]
-#         fechaIngresoTrabajo = self.cleaned_data["fechaIngresoTrabajo"]
-#         sueldo = self.cleaned_data["sueldo"]
-#         horaJornada = self.cleaned_data["horaJornada"]
-#         fechaAfiliacion = self.cleaned_data["fechaAfiliacion"]
-
-#         afiliado = Afiliado(cuit_empleador=cuit_empleador,
-#                             razon_social = razon_social,
-#                             categoria_laboral = categoria_laboral,
-#                             domicilio_empresa = domicilio_empresa,
-#                             localidad_empresa=localidad_empresa,
-#                             rama=rama,
-#                             fechaIngresoTrabajo=fechaIngresoTrabajo,
-#                             sueldo=sueldo,
-#                             horaJornada=horaJornada,
-#                             fechaAfiliacion=fechaAfiliacion,
-#                             estado=1)
-#         afiliado.persona = pearson
-#         self.afiliado = afiliado.save()
-#         return self
-
-#     def __init__(self, instance=None, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         # Calcular la fecha mínima permitida para ser mayor de edad (18 años)
-#         fecha_minima = date.today() - timedelta(days=18*365)
-#         # Formatear la fecha mínima en el formato 'YYYY-MM-DD' requerido por el campo de fecha en HTML
-#         fecha_minima_str = fecha_minima.strftime('%Y-%m-%d')
-#         # Establecer la fecha mínima en el widget del campo de fecha de nacimiento
-#         self.fields['fecha_nacimiento'].widget.attrs['min'] = fecha_minima_str
-#         self.fields['fecha_nacimiento'].help_text = "Debe ser mayor de edad (mayor de 18 años)."
-
-# FormularioAfiliadoCreate.base_fields.update(AfiliadoForm.base_fields)
 ########### Utilizado para el AFILIADO fliadosListView ##############################################
-
 class AfiliadoFilterForm(FiltrosForm):
     persona__nombre = forms.CharField(required=False)
     persona__dni = forms.CharField(required=False)
@@ -209,45 +71,38 @@ class AfiliadoUpdateForm(forms.ModelForm):
     class Meta:
         model = Afiliado
         fields = '__all__'
-        exclude = ['persona','tipo','estado']
-        widgets = {
-            'fechaIngresoTrabajo': forms.DateInput(attrs={'type':'date'}),
-            'fechaAfiliacion': forms.DateInput(attrs={'type':'date'})
-        }
 
-        labels = {
-            'fechaIngresoTrabajo': "fecha de ingreso al trabajo",
-            'fechaAfiliacion': "Fecha de afiliacion"
-        }
+    def __init__(self, *args, **kwargs):
+        print("|-------------------------------------------------------|")
+        print("|------------------   ESTOY AQUI   ---------------------|")
+        print("|-------------------------------------------------------|")
+        super(AfiliadoUpdateForm, self).__init__(*args, **kwargs)
+        # Agrega campos de Persona al formulario
+        persona_fields = ['dni', 'cuil', 'nombre', 'apellido', 'fecha_nacimiento', 'celular', 'direccion', 'nacionalidad', 'mail', 'estado_civil']
+        for field_name in persona_fields:
+            self.fields[field_name] = forms.CharField(
+                required=False,
+                initial=getattr(self.instance.persona, field_name)
+            )
+            self.fields[field_name].widget.attrs['readonly'] = True
 
-class FormularioAfiliadoUpdate(forms.Form):
-    # # ** SE COMENTO ESTA LINEA PARA QUE NO LO CHEQUEARA
-    # def clean_dni(self):
-    #     self.persona = Persona.objects.filter(dni=self.cleaned_data['dni']).first()
-    #     if self.persona is not None and self.persona.es_afiliado:
-    #         raise ValidationError("Ya existe un afiliado activo con ese DNI")
-    #     return self.cleaned_data['dni']
+    def save(self, commit=True):
+        # Actualiza los campos de Afiliado
+        afiliado = super().save(commit=False)
+        # No olvides guardar los campos de Afiliado aquí, si es necesario
+        if commit:
+            afiliado.save()
 
-    def is_valid(self) -> bool:
-        sv = super().is_valid()
-        pv = self.personaForm.is_valid()
-        av = self.afiliadoForm.is_valid()
-        return sv and pv and av
+        return afiliado
 
-    def save(self, commit=False):
-        self.personaForm.save()
-        return self.afiliadoForm.save()
+    def is_valid(self):
+        return super(AfiliadoUpdateForm, self).is_valid()
 
-    def __init__(self, initial = {}, instance=None, *args, **kwargs):
-        persona = instance.persona
-        self.personaForm = PersonaUpdateForm(initial=initial, instance=instance.persona, *args, **kwargs)
-        self.afiliadoForm = AfiliadoUpdateForm(initial=initial, instance=instance, *args, **kwargs)
-        initial = dict(self.personaForm.initial)
-        initial.update(self.afiliadoForm.initial)
-        super().__init__(initial=initial, *args, **kwargs)
-FormularioAfiliadoUpdate.base_fields.update(PersonaUpdateForm.base_fields)
-FormularioAfiliadoUpdate.base_fields.update(AfiliadoUpdateForm.base_fields)
+    def clean(self):
+        cleaned_data = super(AfiliadoUpdateForm, self).clean()
+        return cleaned_data
 
+####-------------------------------------------
 class AfiliadoVer(forms.ModelForm):
     fechaAfiliacion = forms.DateField()
 
