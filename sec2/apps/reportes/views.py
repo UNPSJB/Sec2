e99e7d6e90e55180faf13c3c385edc2814e8f45d
carloslucer <lucero.carlos.ch@gmail.com>
2024-05-10 05:27:18 -0300

    "reportes"

--- sec2/apps/reportes/views.py
+++ sec2/apps/reportes/views.py
@@ -1,12 +1,16 @@
+from decimal import Decimal
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from apps.alquileres.models import Alquiler
 from django.views.generic import TemplateView
 from datetime import datetime
-from collections import Counter
+from collections import Counter, defaultdict
 from django.db.models import Count
 
 from apps.afiliados.models import Afiliado
+from apps.cursos.models import DetallePagoAlumno, Dictado
+from django.db.models import Count
 
 class AfiliadosReportesView(TemplateView):
     template_name = 'reporte_afiliados_historico.html'
@@ -17,9 +21,10 @@ class AfiliadosReportesView(TemplateView):
         
         afiliados_por_mes = Afiliado.objects.all()
         for afiliado in afiliados_por_mes:
-             
             if afiliado.hasta:  # Confirmado
                 month = afiliado.hasta.month
+                cuit_empleador = afiliado.cuit_empleador
+                
                 data_dados_baja[month] += 1
 
             if afiliado.afiliado.fechaAfiliacion != None:
@@ -102,3 +107,70 @@ class reportesView(TemplateView):
             'y_axis_title': 'Total de alquileres',  # Y-axis title
         }
         return context
+
+class ReporteFinanzasCursosViews(TemplateView):
+     template_name = "reporte_cursos_finanzas.html"
+
+
+     def get_pagos(self, year):
+         sumas_por_curso = defaultdict(Decimal)
+         pagos = DetallePagoAlumno.objects.filter(dictado__fecha__year=year)
+         for pago in pagos:
+              nombre_curso = pago.dictado.curso.nombre
+              sumas_por_curso[nombre_curso] += pago.total
+
+         return [{'name': curso, 'data': [[curso, total]]} for curso, total in sumas_por_curso.items()]
+
+     def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+        
+            year = datetime.now().year
+
+            # Asegúrate de convertir el año a entero, ya que los parámetros GET son strings
+            try:
+                year = int(year)
+            except ValueError:
+                return JsonResponse({'error': 'Invalid year format'}, status=400)
+
+            pagos_data = self.get_pagos(year)
+            print(pagos_data)
+            context['year'] = year
+            context['datos'] = pagos_data
+            return context
+
+class ReporteCursosViews(TemplateView):
+    template_name = 'reporte_cursos_torta.html'
+
+    
+    def get_dictados_summary(self, year):
+        return list( Dictado.objects.
+                    filter(fecha__year=year)
+                    .values('curso__nombre')
+                    .annotate(
+            afiliados_count=Count('afiliados'),
+            familiares_count=Count('familiares'),
+            profesores_count=Count('profesores'),
+            alumnos_count=Count('alumnos'),
+ 
+        ).values('curso__nombre', 'afiliados_count', 'familiares_count', 'profesores_count', 'alumnos_count')
+        )
+    
+
+
+    def get_context_data(self, **kwargs) :
+            context = super().get_context_data(**kwargs)
+       
+            year = datetime.now().year
+
+            # Asegúrate de convertir el año a entero, ya que los parámetros GET son strings
+            try:
+                year = int(year)
+            except ValueError:
+                return JsonResponse({'error': 'Invalid year format'}, status=400)
+
+            dictados_data = self.get_dictados_summary(year)
+            context['year'] = year
+            context['datos'] = dictados_data
+
+            print(context)
+            return context
